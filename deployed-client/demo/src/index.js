39f6c1cf8dff65cf63a6d9baa08bd4347b422ddd
Ilya Dierckens <ilya.dierckens@student.howest.be>
2023-05-04 12:53:10 +0200

    Updated API URL

--- deployed-client/demo/src/index.js
+++ deployed-client/demo/src/index.js
@@ -14,7 +14,7 @@ import SendIcon from '@mui/icons-material/Send';
 import { times } from 'lodash';
 
 
-const APIURL = "https://art-workbench-api.herokuapp.com/"
+const APIURL = "https://ilyadierckens.pythonanywhere.com"
 const WEBSERVERURL = "https://bucolic-gnome-8117cd.netlify.app/"
 let miradorInstance;
 
